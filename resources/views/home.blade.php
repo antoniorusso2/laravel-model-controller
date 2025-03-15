0d983b40d822db47eb014d3e9f37b32c43b04687
antoniorusso2 <russoantonio303@outlook.it>
2025-03-15 21:09:47 +0100

    add base layout for cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,13 +10,38 @@
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
+@php
+    // dd($movies); //collection
+@endphp
+
 <body>
+    <main class="main py-5">
 
-    <h1>Homepage</h1>
+        <div class="container">
+            <div class="row title">
+                <div class="col-12 text-center mb-3">
+                    <h1>Movies</h1>
+                </div>
+            </div>
+            <div class="row row-gap-3">
+                @foreach ($movies as $movie)
+                    <div class="col col-sm-6 col-md-4 col-lg-3">
+                        <div class="card">
+                            <h2>{{ $movie->original_title }}</h2>
+                            <div class="card-body">
 
-    @php
-        dd($movies); //collection
-    @endphp
+                                <p class="subtitle">{{ $movie->title }}</p>
+                                <p>{{ $movie->description }}</p>
+                                <p>Data di uscita: {{ $movie->date }}</p>
+                                <p>Voto: {{ $movie->vote }}</p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </main>
+
 </body>
 
 </html>
